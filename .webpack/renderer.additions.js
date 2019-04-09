6d35148e58e5411c90630d1dc7df8e5365bf0ea9
Vishnu R Menon <vishthemenon@gmail.com>
2019-04-09 15:32:47 +0800

    Remove fontawesome and nucleo from vendor files

--- .webpack/renderer.additions.js
+++ .webpack/renderer.additions.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
@@ -10,15 +11,18 @@ module.exports = {
         test: /\.pug$/,
         use: ['pug-loader']
       },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'file-loader',
-        }]
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
       }
     ]
   },
   plugins: [
+    new CopyPlugin([
+      { from: 'static/images', to: 'images' },
+    ]),
     new FriendlyErrorsWebpackPlugin({ clearConsole: process.env === "development" }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../src/renderer/index.pug'),
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+import '@fortawesome/fontawesome-free/js/all'
 import './stylesheets/main.scss'
 
 // Small helpers you might want to keep
